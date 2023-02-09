dcdef6f70386b2db5156d4e5c1638861a7cfb81c
BohuTANG <overred.shuttler@gmail.com>
2023-02-09 20:43:05 +0800

    Bump opendal to 0.26.2

--- common/storages/src/storage.rs
+++ common/storages/src/storage.rs
@@ -20,9 +20,10 @@ use common_configs::FsStorageConfig;
 use common_configs::S3StorageConfig;
 use common_configs::StorageType;
 use common_exceptions::Result;
-use opendal::services::azblob;
-use opendal::services::fs;
-use opendal::services::s3;
+use opendal::services::Azblob;
+use opendal::services::Fs;
+use opendal::services::S3;
+use opendal::Builder;
 use opendal::Operator;
 
 /// init object storage
@@ -36,19 +37,19 @@ pub async fn init_object_storage(conf: &EthConfig) -> Result<Operator> {
 
 /// init_fs_operator will init a opendal fs operator.
 async fn init_fs_storage(cfg: &FsStorageConfig) -> Result<Operator> {
-    let mut builder = fs::Builder::default();
+    let mut builder = Fs::default();
 
     let mut path = cfg.data_path.to_string();
     if !path.starts_with('/') {
         path = env::current_dir().unwrap().join(path).display().to_string();
     }
     builder.root(&path);
-    Ok(Operator::new(builder.build()?))
+    Ok(Operator::new(builder.build()?).finish())
 }
 
 /// init_s3_operator will init a opendal s3 operator with input s3 config.
 async fn init_s3_operator(cfg: &S3StorageConfig) -> Result<Operator> {
-    let mut builder = s3::Builder::default();
+    let mut builder = S3::default();
 
     // Endpoint.
     builder.endpoint(&cfg.endpoint_url);
@@ -69,12 +70,12 @@ async fn init_s3_operator(cfg: &S3StorageConfig) -> Result<Operator> {
     if cfg.enable_virtual_host_style {
         builder.enable_virtual_host_style();
     }
-    Ok(Operator::new(builder.build()?))
+    Ok(Operator::new(builder.build()?).finish())
 }
 
 /// init_azblob_operator will init an opendal azblob operator.
 async fn init_azblob_operator(cfg: &AzblobStorageConfig) -> Result<Operator> {
-    let mut builder = azblob::Builder::default();
+    let mut builder = Azblob::default();
 
     // Endpoint
     builder.endpoint(&cfg.azblob_endpoint_url);
@@ -89,5 +90,5 @@ async fn init_azblob_operator(cfg: &AzblobStorageConfig) -> Result<Operator> {
     builder.account_name(&cfg.account_name);
     builder.account_key(&cfg.account_key);
 
-    Ok(Operator::new(builder.build()?))
+    Ok(Operator::new(builder.build()?).finish())
 }
